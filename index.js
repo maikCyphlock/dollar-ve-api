f7f6f8786126533da82de177377e686eee56838b
maikCyphlock <maikolaguilar656@gmail.com>
2023-01-08 19:43:01 -0400

    refactor

--- index.js
+++ index.js
@@ -2,57 +2,67 @@ import pupperteer from 'puppeteer'
 import { writeFile } from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
-async function main () {
-  const DB_PATH = path.join(process.cwd(), './db/')
-  // Open a Chromium browser. We use headless: false
-  // to be able to watch what's going on.
-  const browser = await pupperteer.launch({
-    headless: true
-  })
-  // Open a new page / tab in the browser.
-  const page = await browser.newPage({
-    bypassCSP: true // This is needed to enable JavaScript execution on GitHub.
-  })
 
-  await page.goto('https://exchangemonitor.net/dolar-venezuela', {
-    waitUntil: 'domcontentloaded'
-  })
+const DB_PATH = path.join(process.cwd(), './db/')
 
-  const links = await page.$$('div.col-xs-12.col-sm-6.col-md-4.col-tabla')
+const saveImagen = async ({ url }) => {
+  const filename = url.match(/([^\/]+)(?=\.\w+$)/g, '')
+  console.log(filename)
+  // const responseImage = await fetch(url)
+  // const arrayBuffer = await responseImage.arrayBuffer()
+  // const buffer = Buffer.from(arrayBuffer)
 
-  const store = []
+  // writeFile(
+  //   path.join(process.cwd(), 'assets', `${filename[0]}.webp`), buffer, err => {
+  //     console.log(err)
+  //   }
 
-  for (const link of links) {
-    const obj = { origin: '', price: '', stock: '', imgUrl: '' }
-    obj.origin = await link
-      .$('h6')
-      .then(e => e.evaluate(node => node.innerText))
+  // )
+  return filename[0]
+}
+const browser = await pupperteer.launch({
+  headless: true
+})
 
-    obj.imgUrl = await link
-      .$('img')
-      .then(e => e.evaluate(node => node.src))
+const page = await browser.newPage({
+  bypassCSP: true
+})
 
-    obj.price = await link
-      .$('p.precio')
-      .then(e => e.evaluate(node => node.innerText))
+await page.goto('https://exchangemonitor.net/dolar-venezuela', {
+  waitUntil: 'domcontentloaded'
+})
 
-    obj.stock = await (
-      await (await link.$$('div'))[4].evaluate(node => node.innerText)
-    )
-      .replace(/\n/g, ' ')
-      .replace(/CAMBIO 24H/, '')
-      .trim()
+const links = await page.$$('div.col-xs-12.col-sm-6.col-md-4.col-tabla')
 
-    store.push(obj)
-  }
+const store = []
 
-  console.log(store)
-  writeFile(`${DB_PATH}/dolar.json`, JSON.stringify(store, null, 2), err => {
-    console.log(err)
-  })
+for (const link of links) {
+  const obj = { origin: '', price: '', stock: '', imgUrl: '' }
+  obj.origin = await link
+    .$('h6')
+    .then(e => e.evaluate(node => node.innerText))
 
-  // Turn off the browser to clean up after ourselves.
-  await browser.close()
+  obj.imgUrl = await link
+    .$('img')
+    .then(e => e.evaluate(node => node.src)).then(e => saveImagen({ url: e }))
+
+  obj.price = await link
+    .$('p.precio')
+    .then(e => e.evaluate(node => node.innerText))
+
+  obj.stock = await (
+    await (await link.$$('div'))[4].evaluate(node => node.innerText)
+  )
+    .replace(/\n/g, ' ')
+    .replace(/CAMBIO 24H/, '')
+    .trim()
+
+  store.push(obj)
 }
 
-main()
+console.log(store)
+writeFile(`${DB_PATH}/dolar.json`, JSON.stringify(store, null, 2), err => {
+  console.log(err)
+})
+
+await browser.close()
