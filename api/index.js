295a486fa34cb8ae2a48c7ee5db01e031cd21325
maikCyphlock <maikolaguilar656@gmail.com>
2023-01-09 19:58:37 -0400

    adding cache

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import dolar from '../db/dolar.json'
 import { Hono } from 'hono'
+import { cache } from 'hono/cache'
 import { serveStatic } from 'hono/serve-static.module'
 
 const app = new Hono()
@@ -8,5 +9,6 @@ const app = new Hono()
 app.get('/', (ctx) => ctx.json(d))
 
 app.get('/static/*', serveStatic({ root: './' }))
+app.get('*', cache({ cacheName: 'dolar-api', cacheControl: 'max-age=3600' }))
 
 export default app
